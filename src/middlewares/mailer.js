ea5595a979652c0450a5fc50b42d41748b246124
Irfan-Julian <irfanjulian23@gmail.com>
2023-02-04 13:53:20 +0700

    middleware mailer

--- src/middlewares/mailer.js
+++ src/middlewares/mailer.js
@@ -3,10 +3,18 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  // auth: {
+  //   user: process.env.MAIL_USERNAME,
+  //   pass: process.env.MAIL_PASSWORD
+  // }
   auth: {
+    type: "OAuth2",
     user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD
-  }
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
 });
 
 module.exports = (email, subject) => {

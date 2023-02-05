a7c7a566b6f3c933ea41a0c4683a0ab2ec750c12
Irfan-Julian <irfanjulian23@gmail.com>
2023-02-06 03:03:50 +0700

    nodemailer error

--- src/middlewares/mailer.js
+++ src/middlewares/mailer.js
@@ -40,7 +40,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD
